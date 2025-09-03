4cf018d24da9fb20661504363a06e77d62ea78a3
AIwithRajesh <choudharyrajesh729@gmail.com>
2025-09-03 09:26:23 +0530

    update: remove build folder from tracking, add example of typedef and resolvers

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,27 @@ import bodyParser from "body-parser";
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-app.use(express.json);
-app.use(bodyParser);
+app.use(express.json());
+// app.use(bodyParser);
+
+app.get("/", (req, res) => {
+  res.json({ msg: "Working" });
+});
 
 // GRAPHQL SERVER
 const gqlserver = new ApolloServer({
-  typeDefs: "",
-  resolvers: {},
+  typeDefs: `
+    type Query {
+      hello: String
+      say(name: String): String
+    }
+  `,
+  resolvers: {
+    Query: {
+      hello: () => "Hey there, I am a graphql server",
+      say: (_, { name }: { name: string }) => `Hey I am ${name}`,
+    },
+  },
 });
 
 await gqlserver.start();
